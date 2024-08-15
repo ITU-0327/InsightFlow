75a9689a470cfd22b09178629e10867aea6e112f
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-08-15 14:59:07 +1000

    ingestion works without running pipeline , but need refactor cannot import class & need to figure out a workaround for metadata extraction

--- api/InsightFlow/VectorDBInteractor.py
+++ api/InsightFlow/VectorDBInteractor.py
@@ -1,5 +1,7 @@
 import shutil
 from typing import List
+
+import aiohttp
 from supabase import Client
 import os
 import requests
@@ -18,6 +20,8 @@ from llama_index.core.ingestion import IngestionPipeline
 from dotenv import load_dotenv
 
 class NodeMetadata(BaseModel):
+    """Model representing metadata extracted from the document."""
+
     tags: List[str] = Field(
         ..., description="A category of the text chunk, can ONLY be one of ['pain points','behaviour','goals','demographics']"
     )
@@ -29,32 +33,28 @@ class NodeMetadata(BaseModel):
         description="Whether the text chunk is suitable to build a customer persona for UX research",
     )
 
-class VectorDBInteractor:
-    _instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(VectorDBInteractor, cls).__new__(cls, *args, **kwargs)
-        return cls._instance
+class VectorDBInteractor:
 
     def __init__(self, supabase_client: Client):
         if not hasattr(self, 'initialized'):
             self.supabase_client = supabase_client
             self.initialized = True
 
-    def ingest_data(self, urls: List[str], project_id:str):
+    async def ingest_data(self, urls: List[str], project_id: str):
         yield "Started processing data"
         load_dotenv(".env.local")
         SUPABASE_DB_CONN: str = os.environ.get("PUBLIC_SUPABASE_DB_CONN_URL")
-
         # LOADING
-        os.makedirs('/ingestion_data', exist_ok=True)
+        ingestion_path = os.path.join(gettempdir(), 'ingestion_data')
+        print(ingestion_path)
+        os.makedirs(ingestion_path, exist_ok=True)
         for url in urls:
-            self._download_file(url, '/ingestion_data')
-
+            await self._download_file(url, ingestion_path)
+        print("extracting file .....")
         file_extractor = {".pdf": PDFReader(), ".csv": CSVReader(concat_rows=False)}
         downloaded_documents = SimpleDirectoryReader(
-            "./ingestion_data", file_extractor=file_extractor
+            ingestion_path, file_extractor=file_extractor
         ).load_data()
 
         # METADATA EXTRACTION
@@ -69,56 +69,59 @@ class VectorDBInteractor:
         openai_program = OpenAIPydanticProgram.from_defaults(
             output_cls=NodeMetadata,
             prompt_template_str="{input}",
-            extract_template_str=EXTRACT_TEMPLATE_STR
+            extract_template_str=EXTRACT_TEMPLATE_STR,
+            description="Program to extract metadata from documents based on NodeMetadata model."
         )
-
+        #
         program_extractor = PydanticProgramExtractor(
             program=openai_program, input_key="input", show_progress=True
         )
-        
+        #
         vector_store = SupabaseVectorStore(
             postgres_connection_string=(
                 SUPABASE_DB_CONN
             ),
-            collection_name=project_id, # Project ID
+            collection_name=project_id,  # Project ID
         )
-        
+        #
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
+        #
         pipeline = IngestionPipeline(
             transformations=[
                 program_extractor
             ],
-        )   
-        
+        )
+
         yield "Extracting Insights ... this might take a while"
-        nodes = pipeline.run(documents=downloaded_documents)
-        
+        print("running pipeline")
+        nodes = pipeline.run(documents=downloaded_documents)  # MAX content length exceeded ERROR
+
         index = VectorStoreIndex(
             nodes,
             storage_context=storage_context,
             show_progress=True,
             use_async=True,
         )
-        
+
         yield "Preparing insights"
-        
+
         # remove all locally stored data & cleanup
         # Cleanup
-        shutil.rmtree('/ingestion_data')
-        yield f"{len(nodes)} Insights ready"
-    
-        
-    def _download_file(self, url: str, dest_folder: str):
+        shutil.rmtree(ingestion_path)
+        print("deleted", ingestion_path)
+        yield f"{"nodes"} Insights ready"
+
+    async def _download_file(self, url: str, dest_folder: str):
         filename = url.split('/')[-1].replace(" ", "_")
         file_path = os.path.join(dest_folder, filename)
-        r = requests.get(url, stream=True, timeout=3000)
-        if r.ok:
-            with open(file_path, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024 * 8):
-                    if chunk:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                r.raise_for_status()
+                with open(file_path, 'wb') as f:
+                    while True:
+                        chunk = await r.content.read(1024 * 8)
+                        if not chunk:
+                            break
                         f.write(chunk)
                         f.flush()
                         os.fsync(f.fileno())
-        else:
-            print(f"Download failed: status code {r.status_code}\n{r.text}")
