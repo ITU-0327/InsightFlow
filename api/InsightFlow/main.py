c829d5628d91077c915854831571ba308ceef126
ITU <itunghsieh@gmail.com>
2024-08-18 21:07:57 +1000

    Specify NLTK data directory and add logging

--- api/InsightFlow/main.py
+++ api/InsightFlow/main.py
@@ -1,11 +1,22 @@
 import nltk
 from nltk.corpus import wordnet
+import os
+import logging
 
-# Check if the 'wordnet' corpus is available, if not, download it
+logging.basicConfig(level=logging.INFO)
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
 try:
     wordnet.ensure_loaded()
+    logging.info("WordNet loaded successfully.")
 except LookupError:
-    nltk.download('wordnet')
+    logging.info("WordNet not found, downloading...")
+    nltk.download('wordnet', download_dir=nltk_data_path)
+    logging.info("WordNet downloaded successfully.")
 
 from llama_index.readers.file import PDFReader
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
