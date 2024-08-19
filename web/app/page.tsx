1862a7ba98b54bf5bc5590312f7bd9ca2d953bd1
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-08-19 20:47:29 +1000

    redirect to dashboard

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { permanentRedirect } from "next/navigation";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -16,7 +17,7 @@ export default async function Index() {
       return false;
     }
   };
-
+  permanentRedirect("/dashboard");
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
