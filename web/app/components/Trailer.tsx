3b935b0d8784f1c8ba9bd46594b23d98259db8c9
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-08-24 00:13:13 +1000

    trailer complete

--- web/app/components/Trailer.tsx
+++ web/app/components/Trailer.tsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
+import NoteCard from "../dashboard/insights/components/NoteCard";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -16,10 +17,13 @@ export default function ScrollTrailer() {
         scrollTrigger: {
           trigger: main.current,
           scrub: 0.5,
-          start: "top 15%", // Adjust as necessary
+          start: "top 40%", // Adjust as necessary
           end: "bottom 30%", // Adjust as necessary
           pin: true, // Pin the container to the center of the screen
           // markers: true, // Uncomment for debugging
+          onEnterBack: () => {
+            gsap.set(".result", { opacity: 0, display: "none" }); // Hide the result when scrolling back up
+          },
         },
       });
 
@@ -56,15 +60,35 @@ export default function ScrollTrailer() {
         ); // Overlap with the first half's animation
       });
 
-      // Fade in the result at the same time as icons fade out
+      // Fade in the result after icons fade out
       timeline.to(
         ".result",
         {
           opacity: 1,
           duration: 1,
           ease: "power2.inOut",
+          onStart: () => {
+            gsap.set(".result", { display: "block" }); // Show the result when the animation starts
+          },
         },
-        0 // Start fading in the result at the same time as icons fade out
+        "-=0.5" // Start fading in the result at the same time as icons fade out
+      );
+
+      // Stagger the animation of the NoteCards
+      timeline.fromTo(
+        ".result-card",
+        {
+          opacity: 0,
+          y: 50, // Starting position (slightly below the normal position)
+        },
+        {
+          opacity: 1,
+          y: 0, // Ending position (normal position)
+          duration: 4,
+          ease: "power2.inOut",
+          stagger: 1, // Stagger time between each card animation
+        },
+        "-=0.5" // Adjust this timing as necessary to control overlap with the previous animation
       );
     },
     { scope: main }
@@ -73,10 +97,8 @@ export default function ScrollTrailer() {
   return (
     <div className="h-screen w-full p-10">
       <div className="mx-auto text-center font-light text-2xl text-neutral-600">
-        Cut through the noise and know
-        <i className="text-purple-700 mx-2 font-medium">exactly</i>what your
-        users want
-      </div>{" "}
+        Analyse accross multiple feedback channels , cut through the noise ...
+      </div>
       <div
         className="section mx-auto w-[80%] h-[300px] flex gap-3 justify-around items-center"
         ref={main as RefObject<HTMLDivElement>}
@@ -143,6 +165,43 @@ export default function ScrollTrailer() {
             alt="Zoom"
             className="mt-[20px]"
           />
+        </div>
+
+        <div className="result opacity-0 absolute text-neutral-600 w-screen flex justify-around">
+          <div className="text-center text-2xl font-light text-neutral-600 mb-10">
+            And know what your users
+            <i className="text-purple-700 mx-2 font-medium">really</i> want.
+          </div>
+          <div className="flex w-8/12 mx-auto">
+            <NoteCard
+              note={{
+                note: "User finds icons confusing",
+                source: "User-Interview#1-Zoom-Transcript.txt",
+                tags: ["pain points"],
+                fullText: "User finds icons confusing",
+              }}
+              className="result-card"
+            />
+            <NoteCard
+              note={{
+                note: "User wants to set notification preferences to reduce notifications",
+                source: "Typeform-pilot-survey.csv",
+                tags: ["pain points", "behaviour"],
+                fullText:
+                  "User wants to set notification preferences to reduce notifications",
+              }}
+              className="result-card "
+            />
+            <NoteCard
+              note={{
+                note: "Need to load results page faster",
+                source: "Intercom-queries.csv",
+                tags: ["pain points"],
+                fullText: "Need to load results page faster",
+              }}
+              className="result-card "
+            />
+          </div>
         </div>
       </div>
       <section className="section h-screen"></section>
