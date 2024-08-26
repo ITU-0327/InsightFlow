60f0be11eb0c963a72f3bdad9a26a1b4d871be27
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-08-26 19:04:27 +1000

    setup posthog analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { PHProvider } from "./providers";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -18,11 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
+      <PHProvider>
+        <body className="bg-background text-foreground">
+          <main className="min-h-screen flex flex-col items-center">
+            {children}
+          </main>
+        </body>
+      </PHProvider>
     </html>
   );
 }
