20c2ed8f6c0947662239751ce5caa4ad5eeeda1d
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-08-26 17:54:16 +1000

    waitlist supabase integration

--- web/app/components/Waitlist.tsx
+++ web/app/components/Waitlist.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import MagicBlob from "@/components/MagicBlob";
 import ShinyButton from "@/components/ShinyButton";
 import Logo from "@/components/ui/logo";
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { handleWaitlistSubmission } from "../wailtlist.actions";
+import Confetti from "react-confetti";
+import { ConfettiButton } from "@/components/magicui/confetti";
 
 const Waitlist = () => {
+  const [email, setEmail] = useState("");
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  const handleSubmit = async (data: FormData) => {
+    const ipres = await fetch("/api/get-ip").then((res) => res.json());
+    const email = data.get("email") as string;
+
+    try {
+      await handleWaitlistSubmission(email, ipres.ip);
+      setShowConfetti(true); // Show confetti after successful submission
+      setEmail(""); // Clear the form
+      setTimeout(() => setShowConfetti(false), 5000); // Stop confetti after 5 seconds
+    } catch (error) {
+      console.error("Failed to submit to the waitlist", error);
+    }
+  };
+
   return (
     <div className="relative h-screen" id="waitlist">
       <MagicBlob />
@@ -14,23 +36,35 @@ const Waitlist = () => {
         <div className="font-medium text-xl text-purple-800">
           Be part of our journey <br />
         </div>
-        <div className="text-xl text-center flex p-2  mb-6 text-indigo-800">
+        <div className="text-xl text-center flex p-2 mb-6 text-indigo-800">
           Join the waitlist and build{" "}
           <Logo className="flex justify-center mr-3" />
           {"  "}
           together!
         </div>
-        <form className="w-full max-w-md">
+        <form
+          className="w-full max-w-md"
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault();
+            const formData = new FormData(e.target as HTMLFormElement);
+            handleSubmit(formData);
+          }}
+        >
           <input
             type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Enter your email"
             required
             className="w-full p-4 mb-4 text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-300 border"
           />
-          <ShinyButton
-            text="Sign me up"
-            className="w-full p-4 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-md transition duration-300"
-          />
+          <ConfettiButton>
+            <ShinyButton
+              text={showConfetti ? "You're in !! 🚀 " : "Sign me up"}
+              className="w-full p-4 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-md transition duration-300"
+            />
+          </ConfettiButton>
         </form>
       </div>
     </div>
